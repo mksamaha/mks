9ffa8b12b256675a4a4ba3af423cb09fab5ec6ce
mksamaha <mk.samaha@gmail.com>
2025-04-24 05:13:06 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+apt update -y && apt upgrade -y
+
+apt install grub2 wimtools ntfs-3g -y
+
+#Get the disk size in GB and convert to MB
+disk_size_gb=$(parted /dev/sda --script print | awk '/^Disk \/dev\/sda:/ {print int($3)}')
+disk_size_mb=$((disk_size_gb * 1024))
+
+#Calculate partition size (25% of total size)
+part_size_mb=$((disk_size_mb / 4))
+
+#Create GPT partition table
+parted /dev/sda --script -- mklabel gpt
+
+#Create two partitions
+parted /dev/sda --script -- mkpart primary ntfs 1MB ${part_size_mb}MB
+parted /dev/sda --script -- mkpart primary ntfs ${part_size_mb}MB $((2 * part_size_mb))MB
+
+#Inform kernel of partition table changes
+partprobe /dev/sda
+
+sleep 30
+
+partprobe /dev/sda
+
+sleep 30
+
+partprobe /dev/sda
+
+sleep 30 
+
+#Format the partitions
+mkfs.ntfs -f /dev/sda1
+mkfs.ntfs -f /dev/sda2
+
+echo "NTFS partitions created"
+
+echo -e "r\ng\np\nw\nY\n" | gdisk /dev/sda
+
+mount /dev/sda1 /mnt
+
+#Prepare directory for the Windows disk
+cd ~
+mkdir windisk
+
+mount /dev/sda2 windisk
+
+grub-install --root-directory=/mnt /dev/sda
+
+#Edit GRUB configuration
+cd /mnt/boot/grub
+cat <<EOF > grub.cfg
+menuentry "windows installer" {
+	insmod ntfs
+	search --set=root --file=/bootmgr
+	ntldr /bootmgr
+	boot
+}
+EOF
+
+cd /root/windisk
+
+mkdir winfile
+
+wget -O win16.iso --user-agent="https://go.microsoft.com/fwlink/p/?LinkID=2195174&clcid=0x409&culture=en-us&country=US"
+
+mount -o loop win16.iso winfile
+
+rsync -avz --progress winfile/* /mnt
+
+umount winfile
+
+wget -O virtio.iso https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/archive-virtio/virtio-win-0.1.271-1/virtio-win-0.1.271.iso
+
+mount -o loop virtio.iso winfile
+
+mkdir /mnt/sources/virtio
+
+rsync -avz --progress winfile/* /mnt/sources/virtio
+
+cd /mnt/sources
+
+touch cmd.txt
+
+echo 'add virtio /virtio_drivers' >> cmd.txt
+
+wimlib-imagex update boot.wim 2 < cmd.txt
+
+reboot
