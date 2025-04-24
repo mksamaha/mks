8738d62566ad1350dd567c6c22fb15e1756465b8
mksamaha <mk.samaha@gmail.com>
2025-04-24 06:41:50 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,78 +1,94 @@
-#!/bin/bash
+apt update
+sudo apt update -y
+sudo apt upgrade -y
+sudo su
+apt install gparted filezilla grub2 wimtools -y
+gparted
+gdisk /dev/sda
+r
 
-set -e
-trap 'echo "An error occurred at line $LINENO. Exiting script." >&2' ERR
+g
 
-echo "Updating system and installing essential packages..."
-apt update -y && apt upgrade -y
-apt install --no-install-recommends grub-pc grub2-common parted gdisk ntfs-3g wget rsync wimtools -y
+p
 
-echo "Partitioning disk..."
-disk_size_gb=$(parted /dev/sda --script print | awk '/^Disk \/dev\/sda:/ {print int($3)}')
-disk_size_mb=$((disk_size_gb * 1024))
-part_size_mb=$((disk_size_mb / 4))
+w
 
-parted /dev/sda --script -- mklabel gpt
-parted /dev/sda --script -- mkpart primary ntfs 1MiB ${part_size_mb}MiB
-parted /dev/sda --script -- mkpart primary ntfs ${part_size_mb}MiB $((2 * part_size_mb))MiB
-
-echo "Refreshing partition table..."
-partprobe /dev/sda
-sleep 5
-
-echo "Formatting partitions..."
-mkfs.ntfs -f /dev/sda1
-mkfs.ntfs -f /dev/sda2
-
-echo "Mounting partitions..."
+Y
 mount /dev/sda1 /mnt
-mkdir -p /root/windisk
-mount /dev/sda2 /root/windisk
-
-echo "Installing GRUB bootloader..."
+cd ~
+mkdir windisk
+mount /dev/sda2 windisk
 grub-install --root-directory=/mnt /dev/sda
+cd /mnt/boot/grub
+nano grub.cfg
 
-echo "Creating grub.cfg..."
-mkdir -p /mnt/boot/grub
-cat <<EOF > /mnt/boot/grub/grub.cfg
-menuentry "Windows Installer" {
-    insmod ntfs
-    search --set=root --file=/bootmgr
-    ntldr /bootmgr
-    boot
+menuentry "windows installer" {
+insmod ntfs
+search --set=root --file=/bootmgr
+ntldr /bootmgr
+boot
 }
-EOF
 
-echo "Downloading Windows ISO..."
-cd /root/windisk
+
+# Navigate to the windisk directory, or create it if it doesn't exist, and navigate into it
+cd /root/windisk || mkdir /root/windisk && cd /root/windisk
+
+# Create a directory named winfile
 mkdir -p winfile
-wget -O win10.iso "https://go.microsoft.com/fwlink/p/?LinkID=2195174&clcid=0x409&culture=en-us&country=US"
+echo "Created winfile directory. Press any key to continue..."
+read -n 1 -s
 
-echo "Mounting Windows ISO and copying files..."
+# Download Windows 10 ISO
+wget -O win10.iso https://go.microsoft.com/fwlink/p/?LinkID=2195174&clcid=0x409&culture=en-us&country=US
+echo "Downloaded win10.iso. Press any key to continue..."
+read -n 1 -s
+
+# Mount the ISO to winfile directory
 mount -o loop win10.iso winfile
+echo "Mounted win10.iso to winfile. Press any key to continue..."
+read -n 1 -s
+
+# Synchronize winfile directory content to /mnt
 rsync -avz --progress winfile/* /mnt
-umount winfile
-rm -rf winfile
+echo "Synced winfile content to /mnt. Press any key to continue..."
+read -n 1 -s
 
-echo "Downloading VirtIO drivers ISO..."
+# Unmount the winfile directory
+umount winfile
+echo "Unmounted winfile. Press any key to continue..."
+read -n 1 -s
+
+# Download VirtIO drivers ISO
 wget -O virtio.iso https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/archive-virtio/virtio-win-0.1.271-1/virtio-win-0.1.271.iso
-mkdir -p winfile
+echo "Downloaded virtio.iso. Press any key to continue..."
+read -n 1 -s
+
+# Mount the VirtIO drivers ISO to winfile directory
 mount -o loop virtio.iso winfile
+echo "Mounted virtio.iso to winfile. Press any key to continue..."
+read -n 1 -s
+
+# Ensure the target directory exists before synchronization
 mkdir -p /mnt/sources/virtio
+echo "Ensured /mnt/sources/virtio exists. Press any key to continue..."
+read -n 1 -s
+
+# Synchronize VirtIO drivers to the specified directory
 rsync -avz --progress winfile/* /mnt/sources/virtio
-umount winfile
-rm -rf winfile
+echo "Synced VirtIO drivers. Press any key to continue..."
+read -n 1 -s
 
-echo "Checking and modifying boot.wim (if available)..."
+# Navigate to the sources directory in /mnt
 cd /mnt/sources
-if [ -f boot.wim ]; then
-    echo "add virtio /virtio_drivers" > cmd.txt
-    wimlib-imagex update boot.wim 2 < cmd.txt
-    echo "boot.wim modified with VirtIO drivers."
-else
-    echo "boot.wim not found. Skipping modification."
-fi
+echo "Navigated to /mnt/sources. Press any key to continue..."
+read -n 1 -s
 
-echo "Installation complete. Rebooting in 10 seconds..."
-sleep 10
-reboot
+# Create a cmd.txt file and write the specified command into it
+echo "add virtio /virtio_drivers" > cmd.txt
+echo "Created and wrote to cmd.txt. Press any key to continue..."
+read -n 1 -s
+
+# Update the boot.wim file based on the cmd.txt instructions
+wimlib-imagex update boot.wim 2 < cmd.txt
+echo "Updated boot.wim. Press any key to continue..."
+read -n 1 -s
