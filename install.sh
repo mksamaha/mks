438697b3df53276293d9db6a8f5fcf02acebeb3f
mksamaha <mk.samaha@gmail.com>
2025-04-24 06:50:05 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,33 +1,64 @@
-apt update
-sudo apt update -y
+#!/bin/bash
 
-sudo su
-apt install gparted filezilla grub2 wimtools -y
-gparted
-gdisk /dev/sda
-r
+apt update -y
 
-g
+apt install grub2 wimtools ntfs-3g -y
 
-p
+#Get the disk size in GB and convert to MB
+disk_size_gb=$(parted /dev/sda --script print | awk '/^Disk \/dev\/sda:/ {print int($3)}')
+disk_size_mb=$((disk_size_gb * 1024))
 
-w
+#Calculate partition size (25% of total size)
+part_size_mb=$((disk_size_mb / 4))
 
-Y
+#Create GPT partition table
+parted /dev/sda --script -- mklabel gpt
+
+#Create two partitions
+parted /dev/sda --script -- mkpart primary ntfs 1MB ${part_size_mb}MB
+parted /dev/sda --script -- mkpart primary ntfs ${part_size_mb}MB $((2 * part_size_mb))MB
+
+#Inform kernel of partition table changes
+partprobe /dev/sda
+
+sleep 30
+
+partprobe /dev/sda
+
+sleep 30
+
+partprobe /dev/sda
+
+sleep 30 
+
+#Format the partitions
+mkfs.ntfs -f /dev/sda1
+mkfs.ntfs -f /dev/sda2
+
+echo "NTFS partitions created"
+
+echo -e "r\ng\np\nw\nY\n" | gdisk /dev/sda
+
 mount /dev/sda1 /mnt
+
+#Prepare directory for the Windows disk
 cd ~
 mkdir windisk
-mount /dev/sda2 windisk
-grub-install --root-directory=/mnt /dev/sda
-cd /mnt/boot/grub
-nano grub.cfg
 
+mount /dev/sda2 windisk
+
+grub-install --root-directory=/mnt /dev/sda
+
+#Edit GRUB configuration
+cd /mnt/boot/grub
+cat <<EOF > grub.cfg
 menuentry "windows installer" {
-insmod ntfs
-search --set=root --file=/bootmgr
-ntldr /bootmgr
-boot
+	insmod ntfs
+	search --set=root --file=/bootmgr
+	ntldr /bootmgr
+	boot
 }
+EOF
 
 
 # Navigate to the windisk directory, or create it if it doesn't exist, and navigate into it
